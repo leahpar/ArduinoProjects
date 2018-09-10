3d5f5444731686a03af41ffc9c5867527c701fb3
leahpar <leahpar@gmail.com>
2018-09-10 10:19:51 +0200

    test réduction bruit 433Mhz

--- MAISON/testDIO/RecordCode/RecordCode.ino
+++ MAISON/testDIO/RecordCode/RecordCode.ino
@@ -15,14 +15,16 @@
 #define DiOremote_PULSEIN_TIMEOUT 1000000
 #define DiOremote_FRAME_LENGTH 64
 // Homeasy start lock : 2725 us (+/- 175 us)
-#define DiOremote_START_TLOW 2550
-#define DiOremote_START_THIGH 2900
+#define DiOremote_START_TLOW 2675-100
+#define DiOremote_START_THIGH 2675+100
+
+#define MARGE 30
 // Homeeasy 0 : 310 us
-#define DiOremote_0_TLOW 250
-#define DiOremote_0_THIGH 450
+#define DiOremote_0_TLOW 275-MARGE
+#define DiOremote_0_THIGH 275+MARGE
 //Homeeasy 1 : 1300 us
-#define DiOremote_1_TLOW 1250
-#define DiOremote_1_THIGH 1500
+#define DiOremote_1_TLOW 1225-MARGE
+#define DiOremote_1_THIGH 1225+MARGE
 
 // --- Variables ---
 const unsigned int RX_PIN = D2;  // 443 MHz rx module pin
@@ -41,40 +43,55 @@ void setup() {
 
 // --- Loop ---
 void loop() {
+  //Serial.print("readRadio... ");
   // Wait for incoming bit
   t = pulseIn(RX_PIN, LOW, DiOremote_PULSEIN_TIMEOUT);
+  //Serial.println(String(t) + "us");
 
   // Only decypher from 2nd Homeeasy lock
   if (t > DiOremote_START_TLOW && t < DiOremote_START_THIGH) {
+Serial.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+code = 0;
 
     for (i = 0 ; i < DiOremote_FRAME_LENGTH ; i++) {
       // Read each bit (64 times)
       t = pulseIn(RX_PIN, LOW, DiOremote_PULSEIN_TIMEOUT);
 
       // Identify current bit based on the pulse length
-      if (t > DiOremote_0_TLOW && t < DiOremote_0_THIGH)
+      if (t > DiOremote_0_TLOW && t < DiOremote_0_THIGH) {
+        Serial.print(0);
         currentBit = 0;
-      else if (t > DiOremote_1_TLOW && t < DiOremote_1_THIGH)
+      } else if (t > DiOremote_1_TLOW && t < DiOremote_1_THIGH) {
+        Serial.print(1);
         currentBit = 1;
-      else
+      } else {
+        Serial.print(".");
+        i--;
+        //continue;
         break;
-      
+      }
       // If bit count is even, check Manchester validity & store in code
       if (i % 2) {
 
         // Code validity verification (Manchester complience)
         if (!(previousBit ^ currentBit))
           break;
-        
+
         // Store new bit
         code <<= 1;
         code |= previousBit;
       }
       previousBit = currentBit;
     }
+  
+  // Send the code via serial
+  //if (i == DiOremote_FRAME_LENGTH) {
+    Serial.println("");
+    Serial.println("code " + String(code));
+    if (code > 1404150000 && code < 1404160000) {
+      Serial.println("OKOKOKOKOKOKOKOKOKOKOKOK");
+    }
+  //}
   }
 
-  // Send the code via serial
-  if (i == DiOremote_FRAME_LENGTH)
-    Serial.println(code);
 }
